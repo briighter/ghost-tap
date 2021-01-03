e15289b9dbe2d8fb47c1d9682d6ce46c9b19cd73
Jason <limaeight@gmail.com>
2021-01-02 19:30:00 -0500

    add driver service file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DriverComponent } from './driver/driver.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DriverComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/driver/driver.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Builder } from 'selenium-webdriver';
+
+@Component({
+  selector: 'app-driver',
+  templateUrl: './driver.component.html',
+  styleUrls: ['./driver.component.css']
+})
+export class DriverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
