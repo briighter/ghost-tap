24c413f886fd2483eb4e8359d66c728e8bc40c54
Jason <limaeight@gmail.com>
2021-01-10 13:35:55 -0500

    Clean workspace

--- test/like.js
+++ test/like.js
@@ -1,12 +1,20 @@
-const { element } = require("protractor");
+const { element, browser } = require("protractor");
+const { protractor } = require("protractor/built/ptor");
 
 describe('Like Photo(s)', function () {
     // Assets
-    var username = element(by.name('username'));
-    var password = element(by.name('password'));
-
-    username.sendKeys("realjasonlima", "password")
+    var username = element(by.css('#loginForm > div > div:nth-child(1) > div > label > input'))
+    var password = element(by.css('#loginForm > div > div:nth-child(2) > div > label > input'))
 
 
+    beforeEach(function () {
+        browser.waitForAngularEnabled(false)
+        browser.get('https://instagram.com')
+        browser.wait(protractor.ProtractorExpectedConditions.presenceOf(username))
+    })
 
+    it('should go to instagram', function () {
+        username.sendKeys("realjasonlima", "password")
+        expect(browser.getTitle()).toEqual('Instagram')
+    })
 });
